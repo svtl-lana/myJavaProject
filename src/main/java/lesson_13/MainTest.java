0d0badf940baaf95a5518256ac83bef27fd68d99
Svetlana <sveta5486819@gmail.com>
2025-07-21 22:18:33 +0300

    Homework 13

---
+++ src/main/java/lesson_13/MainTest.java
@@ -0,0 +1,17 @@
+package lesson_13;
+
+public class MainTest {
+    public static void main(String[] args) {
+        PhoneDirectory PhoneDirectory = new PhoneDirectory();
+
+        PhoneDirectory.add("Gary Carter", "123123123");
+        PhoneDirectory.add("Robert Young", "456456456");
+        PhoneDirectory.add("Richard Flores", "789789789");
+        PhoneDirectory.add("Frank Mullins", "123456789");
+
+        System.out.println("Gary Carter: " + PhoneDirectory.get("Gary Carter"));
+        System.out.println("Robert Young: " + PhoneDirectory.get("Robert Young"));
+        System.out.println("Richard Flores: " + PhoneDirectory.get("Richard Flores"));
+        System.out.println("James Abbott (nonexistent): " + PhoneDirectory.get("James Abbott"));
+    }
+}
---
+++ src/main/java/lesson_13/Words.java
@@ -0,0 +1,33 @@
+package lesson_13;
+
+import java.util.*;
+
+public class Words {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList(
+                "Blueberry", "Strawberry", "Peach", "Blueberry", "Cherry",
+                "Strawberry", "Grape", "Grape", "Blueberry", "Cherry",
+                "Peach", "Apple", "Grape", "Strawberry", "Cherry"
+        );
+
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println("Unique words:");
+        for (String word : uniqueWords) {
+            System.out.println(word);
+        }
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            if (wordCount.containsKey(word)) {
+                int oldCount = wordCount.get(word);
+                wordCount.put(word, oldCount + 1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+        System.out.println("------------------------");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
